41a2c59b7ae4eec786af1b2e830979858720301f
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-02-14 12:37:48 +0100

    Use cross-platform `libuv` and `libpath` methods

--- src/main.c
+++ src/main.c
@@ -1,64 +1,11 @@
 #include <assert.h>
 #include <bare.h>
+#include <path.h>
 #include <uv.h>
-#include <stdlib.h>
 
-#ifdef __linux__
-#include <string.h>
+#ifndef _WIN32
 #include <unistd.h>
 #endif
-
-#define PATH_MAX_STRING_SIZE 4096
-
-#ifdef _WIN32
-static const char SEP = '\\';
-#define PATH_SEP_STRING "\\"
-#else
-static const char SEP = '/';
-#define PATH_SEP_STRING "/"
-#endif
-
-static void
-cd_dot_dot (char *p) {
-  char *lst = p + strlen(p) - 1;
-  while (lst != p && *lst != SEP) {
-    lst--;
-  }
-  *lst = '\0';
-}
-
-static int
-get_binary_path (char *exec_path) {
-#ifdef __linux__
-  ssize_t s = readlink("/proc/self/exe", exec_path, PATH_MAX_STRING_SIZE);
-  if (s < 0) return (int) s;
-  exec_path[s] = '\0';
-  return 0;
-#elif _WIN32
-  DWORD result = GetModuleFileName(NULL, exec_path, PATH_MAX_STRING_SIZE);
-  if (result == 0) return -1;
-  return 0;
-#else
-  uint32_t exec_path_size = PATH_MAX_STRING_SIZE;
-  char tmp[PATH_MAX_STRING_SIZE];
-  int err = _NSGetExecutablePath(tmp, &exec_path_size);
-  if (err < 0) return err;
-  return realpath(tmp, exec_path) == NULL ? -1 : 0;
-#endif
-}
-
-static int
-file_exists (char *filename) {
-#ifdef _WIN32
-  WCHAR tmp[PATH_MAX_STRING_SIZE];
-  mbstowcs(tmp, filename, PATH_MAX_STRING_SIZE);
-  DWORD attr = GetFileAttributesW(tmp);
-  return attr == INVALID_FILE_ATTRIBUTES ? 0 : 1;
-#else
-  struct stat buffer;
-  return stat(filename, &buffer) == 0 ? 1 : 0;
-#endif
-}
 
 int
 main (int argc, char *argv[]) {
@@ -74,27 +21,38 @@ main (int argc, char *argv[]) {
   err = bare_setup(uv_default_loop(), platform, NULL, argc, argv, NULL, &bare);
   assert(err == 0);
 
-  char path[PATH_MAX_STRING_SIZE];
-  err = get_binary_path(path);
+  char path[4096];
+  size_t len = 4096;
+
+  err = uv_exepath(path, &len);
   assert(err == 0);
 
-  // binary is in <root>/by-arch/<host>/bin/<executable>
-  // so we need 4 cd ..
-  cd_dot_dot(path);
-  cd_dot_dot(path);
-  cd_dot_dot(path);
-  cd_dot_dot(path);
+  uv_fs_t req;
+  err = uv_fs_realpath(uv_default_loop(), &req, path, NULL);
+  assert(err == 0);
 
-  int len = strlen(path);
+  strcpy(path, (char *) req.ptr);
 
-  path[len++] = SEP;
+  uv_fs_req_cleanup(&req);
+
+  len = 4096;
+
+  err = path_join(
+    (const char *[]){path, "..", "..", "..", "..", NULL},
+    path,
+    &len,
+    path_behavior_system
+  );
+  assert(err == 0);
+
+  path[len++] = path_separator_system;
   path[len++] = 'b';
   path[len++] = 'o';
   path[len++] = 'o';
   path[len++] = 't';
   path[len++] = '.';
 
-  int dot = len;
+  size_t dot = len;
 
   path[len++] = 'b';
   path[len++] = 'u';
@@ -104,7 +62,11 @@ main (int argc, char *argv[]) {
   path[len++] = 'e';
   path[len++] = '\0';
 
-  if (!file_exists(path)) {
+  err = uv_fs_access(uv_default_loop(), &req, path, F_OK, NULL);
+
+  uv_fs_req_cleanup(&req);
+
+  if (err != 0) {
     path[dot++] = 'j';
     path[dot++] = 's';
     path[dot++] = '\0';
