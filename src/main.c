16a63cdcbf7d6d9da47f09b05a2f4f2df903b239
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-06-06 02:17:28 +0800

    Add support for the restart exit code

--- src/main.c
+++ src/main.c
@@ -8,6 +8,8 @@
 #include <unistd.h>
 #endif
 
+#define RESTART_EXIT_CODE 75
+
 int
 main (int argc, char *argv[]) {
   int err;
@@ -18,67 +20,70 @@ main (int argc, char *argv[]) {
   err = js_create_platform(uv_default_loop(), NULL, &platform);
   assert(err == 0);
 
-  bare_t *bare;
-  err = bare_setup(uv_default_loop(), platform, NULL, argc, argv, NULL, &bare);
-  assert(err == 0);
-
-  char path[4096];
-  size_t len = 4096;
-
-  err = uv_exepath(path, &len);
-  assert(err == 0);
-
-  uv_fs_t req;
-  err = uv_fs_realpath(uv_default_loop(), &req, path, NULL);
-  assert(err == 0);
-
-  strcpy(path, (char *) req.ptr);
-
-  uv_fs_req_cleanup(&req);
-
-  len = 4096;
-
-  err = path_join(
-    (const char *[]){path, "..", "..", "..", "..", NULL},
-    path,
-    &len,
-    path_behavior_system
-  );
-  assert(err == 0);
-
-  path[len++] = path_separator_system;
-  path[len++] = 'b';
-  path[len++] = 'o';
-  path[len++] = 'o';
-  path[len++] = 't';
-  path[len++] = '.';
-
-  size_t dot = len;
-
-  path[len++] = 'b';
-  path[len++] = 'u';
-  path[len++] = 'n';
-  path[len++] = 'd';
-  path[len++] = 'l';
-  path[len++] = 'e';
-  path[len++] = '\0';
-
-  err = uv_fs_access(uv_default_loop(), &req, path, F_OK, NULL);
-
-  uv_fs_req_cleanup(&req);
-
-  if (err != 0) {
-    path[dot++] = 'j';
-    path[dot++] = 's';
-    path[dot++] = '\0';
-  }
-
-  err = bare_run(bare, path, NULL);
-  assert(err == 0);
-
   int exit_code;
-  err = bare_teardown(bare, &exit_code);
-  assert(err == 0);
+
+  do {
+    bare_t *bare;
+    err = bare_setup(uv_default_loop(), platform, NULL, argc, argv, NULL, &bare);
+    assert(err == 0);
+
+    char path[4096];
+    size_t len = 4096;
+
+    err = uv_exepath(path, &len);
+    assert(err == 0);
+
+    uv_fs_t req;
+    err = uv_fs_realpath(uv_default_loop(), &req, path, NULL);
+    assert(err == 0);
+
+    strcpy(path, (char *) req.ptr);
+
+    uv_fs_req_cleanup(&req);
+
+    len = 4096;
+
+    err = path_join(
+      (const char *[]){path, "..", "..", "..", "..", NULL},
+      path,
+      &len,
+      path_behavior_system
+    );
+    assert(err == 0);
+
+    path[len++] = path_separator_system;
+    path[len++] = 'b';
+    path[len++] = 'o';
+    path[len++] = 'o';
+    path[len++] = 't';
+    path[len++] = '.';
+
+    size_t dot = len;
+
+    path[len++] = 'b';
+    path[len++] = 'u';
+    path[len++] = 'n';
+    path[len++] = 'd';
+    path[len++] = 'l';
+    path[len++] = 'e';
+    path[len++] = '\0';
+
+    err = uv_fs_access(uv_default_loop(), &req, path, F_OK, NULL);
+
+    uv_fs_req_cleanup(&req);
+
+    if (err != 0) {
+      path[dot++] = 'j';
+      path[dot++] = 's';
+      path[dot++] = '\0';
+    }
+
+    err = bare_run(bare, path, NULL);
+    assert(err == 0);
+
+    err = bare_teardown(bare, &exit_code);
+    assert(err == 0);
+  } while (exit_code == RESTART_EXIT_CODE);
 
   err = js_destroy_platform(platform);
   assert(err == 0);
